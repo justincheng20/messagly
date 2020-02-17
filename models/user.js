1d1bda99276293f1a9be1ee73f06684e210d6880
justincheng20 <justincheng20@gmail.com>
2020-02-17 12:03:08 -0800

    Started editing user class

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 /** User class for message.ly */
-
+const db = require("../db")
 
 
 /** User of the site. */
@@ -10,7 +10,14 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({ username, password, first_name, last_name, phone }) {
+    const results = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone) 
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING (username, password, first_name, last_name)`),
+      [username, password, first_name, last_name, phone];
+    return results.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
@@ -23,7 +30,11 @@ class User {
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const results = await db.query(
+      `SELECT username, first_name, last_name, phone FROM users`);
+    return results.rows;
+  }
 
   /** Get: get user by username
    *
@@ -34,7 +45,20 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const results = await db.query(
+      `SELECT username, 
+            first_name, 
+            last_name, 
+            phone, 
+            join_at, 
+            last_login_at 
+            FROM users 
+            WHERE username = $1`,
+      [username]
+    );
+    return results.rows[0]
+  }
 
   /** Return messages from this user.
    *
@@ -44,7 +68,21 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {
+    const results = await db.query(
+      `SELECT id, 
+      username, 
+      first_name, 
+      last_name, 
+      phone, 
+      body, 
+      sent_at, 
+      read_at 
+      FROM messages
+      JOIN users
+      ON username = from_username`
+    )
+  }
 
   /** Return messages to this user.
    *
